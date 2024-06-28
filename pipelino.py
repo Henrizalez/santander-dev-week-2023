9913f4f3f004ad4be562a070989f2a24bb1be0ce
Henrizalez <henrique026@gmail.com>
2024-06-28 14:00:43 -0300

    Projeto_Santander_Analise_Dados

---
+++ pipelino.py
@@ -0,0 +1,69 @@
+#Bibliotecas
+
+import pandas as pd
+import requests
+import json
+import openai
+     
+#Importando e Lendo Dataset
+
+df = pd.read_csv('user_id.csv')
+df
+user_ids = df['UserID'].tolist()
+print(user_ids)
+
+#Extract (Criando variável para a url)
+
+sdw2023_api_url = 'https://sdw-2023-prd.up.railway.app'
+
+#Recuperando user_ids pela API
+
+def get_user(id):
+  response = requests.get(f'{sdw2023_api_url}/users/{id}')
+  return response.json() if response.status_code == 200 else None
+
+users = [user for id in user_ids if (user := get_user(id)) is not None]
+print(json.dumps(users, indent=2))
+
+#Transform
+
+openai.api_key =''
+
+#Chamando a API
+
+def generate_ai_news(user):
+  completion = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+      {
+          "role": "system",
+          "content": "Você é um especialista em markting bancário."
+      },
+      {
+          "role": "user",
+          "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
+      }
+    ]
+  )
+  return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+  news = generate_ai_news(user)
+  print(news)
+  user['news'].append({
+      "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+      "description": news
+  })
+  
+#Load
+
+def update_user(user):
+  response = requests.put(f"{sdw2023_api_url}/users/{user['id']}", json=user)
+  return True if response.status_code == 200 else False
+
+for user in users:
+  success = update_user(user)
+  print(f"User {user['name']} updated? {success}!")
+  
+
+     
